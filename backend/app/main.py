040d693a4993ec40b13eea965c797989c0a53705
Julian Bauer-Kong <jbauerkong@gmail.com>
2025-09-13 11:40:46 -0400

    add localhost:3000 as an allowed origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from app.api.routes import router as api_router
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Backend API", version="1.0.0")
 
+# Need to figure this out later
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Include API routes
-app.include_router(api_router, prefix="/api")
+app.include_router(api_router)
 
 @app.get("/")
 async def root():
